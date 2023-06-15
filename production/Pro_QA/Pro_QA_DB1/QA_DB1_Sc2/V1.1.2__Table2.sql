90455285f6b3328e5d5f833afb1cf767bdbd3de6
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-15 14:50:38 +0530

    Tables

---
+++ production/Pro_QA/Pro_QA_DB1/QA_DB1_Sc2/V1.1.2__Table2.sql
@@ -0,0 +1,13 @@
+USE DATABASE Pro_QA_DB1;
+CREATE SCHEMA QA_DB1_Sc2;
+CREATE TABLE QA_1
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO QA_1
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;
