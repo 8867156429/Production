8c275c9ba7923cb852b9df0f1c24105a91b3a4ee
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-16 19:27:51 +0530

    Tables

---
+++ production/Prd_stg/Prd_stg_DB1/stg_DB1_Sc3/V1.1.9__Table9.sql
@@ -0,0 +1,13 @@
+USE DATABASE Prd_stg_DB1;
+CREATE SCHEMA stg_DB1_Sc3;
+CREATE TABLE stg_1
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO stg_1
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;
